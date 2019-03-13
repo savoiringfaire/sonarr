87e2d1af4aa2136a6c7944e158fa913cdfe76212
Marcus <marcus@microserve.io>
2019-03-13 16:10:07 +0000

    Add options to manage packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,17 @@
 #   include sonarr
 class sonarr (
   String $gpg_id = '0xA236C58F409091A18ACA53CBEBFF6B99D9B78493',
-  String $gpg_keyserver = 'keyserver.ubuntu.com:80'
+  String $gpg_keyserver = 'keyserver.ubuntu.com:80',
+  Boolean $manage_gnupg = true,
+  Boolean $manage_cacertificates = true,
 ){
-  package { 'gnupg': }
-  package { 'ca-certificates': }
+  if ($manage_gnugp) {
+    package { 'gnupg': }
+  }
+
+  if ($manage_cacertificates) {
+    package { 'ca-certificates': }
+  }
 
   apt::key { 'sonarr':
     id     => $gpg_id,
